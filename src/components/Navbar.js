2b36e121b7a1f28b0a1fb9902edc454221152075
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-06-13 01:43:34 -0700

    navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,45 @@
 import React from 'react';
+// react router 
+import { NavLink } from "react-router-dom";
 // scss 
 import Styles from "../assets/styles/components_styles/Navbar.module.scss";
+// images 
+import Logo from "../assets/images/logo-header.svg";
+// react icons 
+import { BiSearch } from "react-icons/bi";
+import { BiMenu } from "react-icons/bi";
 
 const Navbar = () => {
   return (
     <>
-      navbar
+      <div className={Styles.navbarContainer}>
+        <img className={Styles.siteLogo} src={Logo} alt="site_logo" />
+        
+        <div className={Styles.searchmenusignupIcon}>
+        <nav className={Styles.ulNavbar}>
+          <ul>
+            <li>
+              <NavLink to="/" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/portfolio" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Portfolio</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Blog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className={navData => navData.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink}>Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+          <BiSearch className={Styles.serchIcon} />
+          <button className={Styles.signupBtn}>Sign Up</button>
+          <BiMenu className={Styles.menuIcon} />
+        </div>
+      </div>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// react router 
+import { BrowserRouter } from "react-router-dom";
+// scss 
+import "./index.scss";
+// bootstrap 
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
