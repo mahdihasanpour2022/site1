3f5ef57303424289386c55699050c6da99224871
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-06-13 05:41:14 -0700

    header right styled completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { Link } from "react-router-dom";
 // scss 
 import Styles from "../assets/styles/components_styles/Header.module.scss";
+// images 
+import girl from "../assets/images/hero-img.png";
+import user1 from "../assets/images/users/user1.jpg";
+import user2 from "../assets/images/users/user2.jpg";
+import user3 from "../assets/images/users/user3.jpg";
 
 const Header = () => {
   return (
@@ -22,7 +27,18 @@ const Header = () => {
           </div>
         </div>
         <div className={Styles.headerRight}>
-
+          <div className={Styles.orangePLane}></div>
+          <div className={Styles.whiteBox}>
+            <p>+2.1M</p>
+            <div className={Styles.users}>
+              <img src={user1} alt="user1" />
+              <img src={user2} alt="user2" />
+              <img src={user3} alt="user3" />
+            </div>
+          </div>
+          <div className={Styles.imageBox}>
+            <img src={girl} alt="girl" />
+          </div>
         </div>
       </div>
     </>
