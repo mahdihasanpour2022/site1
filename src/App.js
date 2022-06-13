54cf5239589cb75520e36bc43cf5dd46362bb09b
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-06-12 23:00:03 -0700

    initial first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// components 
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// scss 
+import Styles from "../assets/styles/components_styles/Navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <>
+      navbar
+    </>
+  );
+};
+
+export default Navbar;
